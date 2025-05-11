d0259502404d183906c24cbf0ab620ec28de1085
Bernard Théoden <theodenbernard1203@gmail.com>
2025-05-11 12:37:47 +0200

    FEAT: my first commit

---
+++ quest_01/ex01/my_first_variable_integer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int person_age;
+
+int main(){
+    person_age = 34;
+
+    printf("%d\n", person_age);
+    return 0;
+}
---
+++ quest_01/ex02/my_first_variable_char.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+     char my_lettre = 'c';
+
+    printf("%c\n", my_lettre);
+    return 0;
+}
